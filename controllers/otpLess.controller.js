44e19eefde0f7c4e3a25ac4f78874904b78878ce
Rammaho <rammah.y7y@gmail.com>
2022-09-07 14:25:02 +0530

    adding comments about used URLs

--- controllers/otpLess.controller.js
+++ controllers/otpLess.controller.js
@@ -3,12 +3,12 @@ const express = require ('express')
 const axios = require('axios')
 
 const getSignupUrl = async (req, res)  => {
-
+// calling the URL of OTPless for signing up 
     const response = await axios.get('https://api.otpless.com/api/v1/user/getSignupUrl', {
     headers: {
         clientid: process.env.clientid,
         clientsecret: process.env.clientsecret,
-    },    
+    },    // required headers for signing app ( clientid, and client secret after creating new application with OTPless.com)
 }).then(response => {
     res.status(200).json({data: response.data})
 }).catch(err => res.status(400).json({data: err}))
@@ -16,8 +16,8 @@ const getSignupUrl = async (req, res)  => {
 
 
 const getUserDetails = async(req, res) => {
-    const response = await axios.post('https://api.otpless.com/api/v1/user/getUserDetails',
-        {token: process.env.token },
+    const response = await axios.post('https://api.otpless.com/api/v1/user/getUserDetails', // URL for fetching user details (name, phoneNumber) 
+        {token: process.env.token }, // token is required and it can be taken by fisiting the signup URL.
     {
         headers: {
             clientid : process.env.clientid,
